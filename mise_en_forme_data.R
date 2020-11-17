4ab76990c1e684892a1c2eef50416459a78d3093
SBareille <72937199+SBareille@users.noreply.github.com>
2020-11-17 22:33:07 +0100

    Update mise_en_forme_data.R

--- mise_en_forme_data.R
+++ mise_en_forme_data.R
@@ -7,7 +7,7 @@ library(maptools)
 library(raster)
 library(SDMTools)
 #library(openxlsx)
-# les librairies suivantes ne sont pas forcemment utiles, à voir
+# les librairies suivantes ne sont pas forcemment utiles, Ã  voir
 library(gridExtra)
 library(biomod2) 
 library(PresenceAbsence)
@@ -23,10 +23,10 @@ library(sf)
 #library(Rmpfr)
 
 ### Set working directory 
-setwd("C:/Users/flori/Desktop/cours/MLB")
+# setwd("C:/Users/flori/Desktop/cours/MLB")
 
 # Download occurrences from OBIS  
-# on garde seulement les colonnes coordonnées et date d'observation (pour pouvoir récupérer les données environnementales correspondantes)
+# on garde seulement les colonnes coordonnÃ©es et date d'observation (pour pouvoir rÃ©cupÃ©rer les donnÃ©es environnementales correspondantes)
 
 OCC <- robis::occurrence(scientificname = "Chaetodon rainfordi" ) # Download, be carefull of the firewall
 if (dim(OCC)[1] > 0) {
@@ -35,27 +35,28 @@ if (dim(OCC)[1] > 0) {
 }
 
 
-# enlever les lignes où l'on n'a pas de date d'observation, ou si le poisson a été observé avant 1985 (car pas assez d'observations par decades)
+# enlever les lignes oÃ¹ l'on n'a pas de date d'observation, ou si le poisson a Ã©tÃ© observÃ© avant 1985 (car pas assez d'observations par decades)
 OCC <- na.omit(OCC, cols='year')
 OCC <- OCC[-which(OCC$year<1985),]
-OCC <- OCC[-which(OCC$year>2012),] # car on n'a pas de températures dispo après 2012. Même sur NOAA. 
-                                  # doit on vraiment enlever ces poissons ? car il y en a quand même environ 900 (sur environ 4900)
+OCC <- OCC[-which(OCC$year>2012),] # car on n'a pas de tempÃ©ratures dispo aprÃ¨s 2012. MÃªme sur NOAA. 
+# doit on vraiment enlever ces poissons ? car il y en a quand mÃªme environ 900 (sur environ 4900)
 
 
-# associer pour chaque année la decade correspondante pour pouvoir récupérer température et salinité
+# associer pour chaque annÃ©e la decade correspondante pour pouvoir rÃ©cupÃ©rer tempÃ©rature et salinitÃ©
 OCC$decade <- NA
 OCC$decade <- ifelse(OCC$year >= 1985 & OCC$year <= 1994, 1, OCC$decade)
 OCC$decade <- ifelse(OCC$year >= 1995 & OCC$year <= 2004, 2, OCC$decade)
 OCC$decade <- ifelse(OCC$year >= 2004 & OCC$year <= 2012, 3, OCC$decade)
 
-# # Importing boudries coordinates 
+# Importing boundries coordinates 
+# This limit corresponds to the area studied, for which we have information on its geomorphology
 
 boundaries <- readOGR( 
   dsn= "3dgbr_geomorph/shape", 
   layer = "qld_gbrwha_cscz",
   verbose=FALSE)
 
-
+# We plot fishes to see if some of them are out of boundaries
 plot(OCC$longitude, OCC$latitude, xlim=c(120,155), ylim=c(-28,-14))
 par(new=T)
 plot(coordinates(boundaries)[[1]][[1]],pch=20, xlim=c(120,155), ylim=c(-28,-14))
@@ -69,111 +70,13 @@ par(new=T)
 plot(coordinates(boundaries)[[1]][[1]],pch=20, xlim=c(120,155), ylim=c(-28,-14))
 
 #Now all the fishes are in our boundaries
-
-#Plot occurrences
-# a faire. les lignes en dessous ne sont que des exemples
-# plot(getMap(resolution = "coarse"),col="gray")
-# plot(OCC,add=T,col=2,cex=0.3,pch=15)
-
-# enlever les poissons qui sont en dehors de notre environnement d'étude et ceux qui ont longitudes et latitudes =0
-# pas possible encore car on a besoin de manipuler ces jeux de données avant.
-# on va plutot de récupérer les infos geometric pour nos coord d'occurences, et si c'est pas possible c'est qu'on n'est pas dans la zone (supprimer cette occurence)
-
-
-##### on va maintenant créer la base de notre dataframe absences
-# on ajoutera les colonnes variables pour chaque data frame en parallèle ensuite
-# mais pour créer les absences il faut rester dans notre zone d'étude
-# pour cela on va se servir des infos locales comme les données géomorphiques de la barrière du corail
-
-
-# Importing geomorphic features
-# Ce csv a été créé à partir des données sur https://www.deepreef.org/bathymetry/65-3dgbr-bathy.html 
-# pour chaque fichier shapefile les commandes suivantes ont été effectuées, puis tout a été fusionné : 
-# my_spdf_cay <- readOGR( 
-#   dsn= "3dgbr_geomorph/shape", 
-#   layer = "coralsea_cay",
-#   verbose=FALSE)
-# cc = coordinates(my_spdf_cay)
-# geomorphic_1 = data.frame(cc)
-# colnames(geomorphic_1) = c("lon","lat")
-# geomorphic_1$type = "cay"
+#The Occurrence data are ready to be used
 
 
 
-# faire représentation graphique ? 
-# Exemple: on trace les coordonnÃ©es des points du canyon
-#plot(coordinates(my_spdf_canyon),pch=20)
-# On ajoute le plateau
-#points(coordinates(my_spdf_plateau),pch=17,col="orange")
-# Faire un plot représentant tout le geomorphic ??
-# plot(coordinate(geomorphic))
-
-# Grace aux coordonnées de notre zone où a des informations sur le geomorph on a l'étendu de notre zone d'étude
-# Avant de créer les absences, on va enlever les poissons qui ont été observés trop loins de notre zone d'étude en se servant de la distance minimiale qu'ils ont avec celle-ci
-# si à plus de 100 km de la zone on supprime.
-#for (k in 1:nrow(OCC)){
-#  d = sqrt((OCC$longitude[k]-geomorphic$lon)^2 + (OCC$latitude[k]-geomorphic$lat)^2)
-#  if (min(d) > 100000/(100000 * 1.04)){
-#    OCC <- OCC[-k,]
-#    }
-#}
-# le problème c'est qu'il y a beaucoup de données qui sont à plus de 100 km d'une donnée locale geomorphic
-# en fait on n'a pas assez de données geomorphic pour s'en servir pour délimiter la zone
-# il faudrait qu'on se serve des infos de boundaries mais comme il ne s'agit pas d'une zone rectangulaire on n'arrive pas faire des conditions sur ces limites
-
-
-# # Importing boudries coordinates 
-
- boundaries <- readOGR( 
-   dsn= "3dgbr_geomorph/shape", 
-   layer = "qld_gbrwha_cscz",
-   verbose=FALSE)
- 
- #boundaries <- coordinates(boundaries)[[1]][[1]]
- #plot(boundaries,pch=20)
- plot(coordinates(boundaries)[[1]][[1]], xlim=c(140,160), ylim=c(-25,-10), col='green')
- 
-
- # ajouter la limite sud (à latitude = -24.5)
- # for (longit in seq(152.042,154,by=0.001)){
- #  boundaries <- rbind(boundaries, c(longit,-24.5))
- #}
- #plot(boundaries,pch=20)
-
-
-############# Création du date frame absence ##############
-
-# définir autour de nos coordonnées d'occurence d'observation une zone dans laquelle il ne sera pas possible de piocher les absences
-
-
-# ATTENTION !! Il faut qu'on reparte sur autre chose que juste les coords de geomorphic parce qu'il n'y a pas assez de couples de coordonnées dispos ! juste 1059 !
-# Se servir de boundaries
-# quand on créera le tableau ABS il faudra créer les mêmes colonnes que OCC pour pouvoir les fusionner ensuite
-
-
-
-
-
-# les coordonnees sont des degres d嶰imaux. 
-# une variation � partir du 5eme chiffre apres la virgule correspond a une variation d'environ 1.11 m a l'equateur.
-# dans un soucis de simplification, et de part la petitesse de la superficie des zones calculees autour des occurrences,
-# nous allons negliger la courbure de la Terre et considerer une variation de 1.11 m dans la Grande Barriere de corail.
-
-# Rayon de la Terre : 6400 km
-# La Grande Barri鋨e de corail se situe environ a 20 degres Sud
-# Rayon de la Terre dans le plan passant par la latitude 20 degres Sud et parallele au plan de l'equateur : 6400 * cos(20) = 6014 km
-# Le rapport entre la variation en metres entre la latitude a l'equateur et a la latitude de laGrande Barriere de corail est : 6014/6400 = 0.93969
-# La variation au niveau de la Grande Barriere de corail est donc de 1.11 * 0.93969 = 1.04
-
-lim = 1000/(100000 * 1.04) #on d嶨init les limites des zones o� l'on ne fera pas les tirages des pseudos absences avec un rayon de 1 km autour de l'occurrence, que l'on convertit en variation de degres decimaux. 
-#cette zone represente l'aire ou les poissons n'ont pas etes observes mais sont consideres comme existants
-
-
-#####################################################
-#####################################################
-#creation jeu de donnees pseudos absences
-#####################################################
-#####################################################
+##### on va maintenant crÃ©er la base de notre dataframe absences
+# on ajoutera les colonnes variables pour chaque data frame en parallÃ¨le ensuite
+# mais pour crÃ©er les absences il faut rester dans notre zone d'Ã©tude. Nous allons nous servir de boundaries
 
 # initialisation des coordonnees de la zone d'etude des 2 premieres decennies
 coord_bnd <- coordinates(boundaries)[[1]][[1]]
@@ -199,6 +102,29 @@ absd3 <- matrix(as.numeric(NA), nrow = nrow(OCCd3), ncol = 2)
 plot(OCCd1$longitude, OCCd1$latitude, xlim=c(140,160), ylim=c(-25,-10), col='green')
 
 # definition des bordures de la zone sans pseudos absences
+# Ces limites sont définies pour les deux premières décennies (POURQUOI ?)
+
+
+
+# texte pour expliquer lim ! le mettre à endroit approprié 
+
+# les coordonnees sont des degres då¶°imaux. 
+# une variation ï¿½ partir du 5eme chiffre apres la virgule correspond a une variation d'environ 1.11 m a l'equateur.
+# dans un soucis de simplification, et de part la petitesse de la superficie des zones calculees autour des occurrences,
+# nous allons negliger la courbure de la Terre et considerer une variation de 1.11 m dans la Grande Barriere de corail.
+
+# Rayon de la Terre : 6400 km
+# La Grande Barrié‹¨e de corail se situe environ a 20 degres Sud
+# Rayon de la Terre dans le plan passant par la latitude 20 degres Sud et parallele au plan de l'equateur : 6400 * cos(20) = 6014 km
+# Le rapport entre la variation en metres entre la latitude a l'equateur et a la latitude de laGrande Barriere de corail est : 6014/6400 = 0.93969
+# La variation au niveau de la Grande Barriere de corail est donc de 1.11 * 0.93969 = 1.04
+
+lim = 1000/(100000 * 1.04) #on då¶¨init les limites des zones oï¿½ l'on ne fera pas les tirages des pseudos absences avec un rayon de 1 km autour de l'occurrence, que l'on convertit en variation de degres decimaux. 
+#cette zone represente l'aire ou les poissons n'ont pas etes observes mais sont consideres comme existants
+
+
+
+
 
 # limite sud
 lim_south <- OCCd1[which.max(OCCd1$longitude),1:2]
@@ -242,7 +168,7 @@ j <- lim_mid[2]
 while(i < lim_south[1]){
   i <- i + 0.01
   j <- j - pas
-    coord_bnd <- rbind(coord_bnd, c(i,j))
+  coord_bnd <- rbind(coord_bnd, c(i,j))
 }
 plot(coord_bnd, xlim=c(140,160), ylim=c(-25,-10), col='green')
 
@@ -261,10 +187,10 @@ while(i < lim_mid[1]){
 plot(coord_bnd, xlim=c(140,160), ylim=c(-25,-10), col='green')
 
 
-# polygone de la zone o� il ne doit pas y avoir de pseudos absences pour la 1ere decennie
+# polygone de la zone où il ne doit pas y avoir de pseudos absences pour la 1ere decennie
 poly_OCC <- Polygon(coords = matrix(c(bound_south, lim_mid, lim_north, bound_north, bound_south), nrow = 5, ncol = 2, byrow = T))
 
-# polygone o� se feront les tirages des pseudos absences dd la 1ere decennie
+# polygone où se feront les tirages des pseudos absences dd la 1ere decennie
 Polys <- Polygon(coords = as.matrix(coord_bnd, nrow = nrow(coord_bnd), ncol = 2))
 
 
@@ -293,7 +219,7 @@ for(i in 1:nrow(OCCd2)){
 # on isole les occurrences qui ne sont pas dans la meme zone que les 2 premieres decennies
 area_d3 <- OCCd3[which(OCCd3$longitude > 152 & OCCd3$latitude > -22),1:2]
 
-# on redefinit les bordures de la zone de presence des poissons de la 3eme decennie
+### on redefinit les bordures de la zone de presence des poissons de la 3eme decennie
 
 # limite sud
 lim_south_d3 <- c(area_d3[which.max(area_d3$longitude),1], area_d3[which.max(area_d3$latitude),2])
@@ -356,15 +282,17 @@ for(i in 1:nrow(OCCd3)){
   absd3[i,] <- coordinates(abs)[1,]
 }
 
+
+
+
+
 #on rassemble les pseudos absences
 ABS <- rbind(absd1,absd2,absd3)
 ABS <- data.frame(ABS)
 colnames(ABS) <- c('longitude', 'latitude')
 write.csv(ABS,"ABS.csv", row.names = TRUE)
 
-
-
-# on ajoute une colonne presence remplie de 0 ou 1 pour pouvoir ensuite lors de notre analyse assimiler que 1=présence et 0=absence
+# on ajoute une colonne presence remplie de 0 ou 1 pour pouvoir ensuite lors de notre analyse assimiler que 1=prÃ©sence et 0=absence
 OCC <- OCC[,-3]
 OCC$presence <- NA
 OCC$presence <- rep(1, nrow(OCC))
@@ -373,22 +301,39 @@ ABS$decade <- OCC$decade
 ABS$presence <- NA
 ABS$presence <- rep(0, nrow(ABS))
 
-
-# fusionner les deux tableaux OCC et ABS pour ensuite analyser facilement les données
-# la fusion avant le récupérage des variables permet d'écrire moins de lignes de code, mais si besoin on peut aussi récupérer les variables pour chaque tableau séparément.
-
+# fusionner les deux tableaux OCC et ABS pour ensuite analyser facilement les donnÃ©es
 DATA <- rbind(OCC, ABS)
 # ainsi PA$resp   qu'on a dans le script de l'article sera  DATA$presence   
 
 
-############# Ajout des variables pour nos données occurences et absences à partir des coordonnées  ##############
+############# Ajout des variables pour nos donnÃ©es occurences et absences Ã  partir des coordonnÃ©es  ##############
 
-# On commence par ajouter la variable geomorphic (comme on l'a déjà chargé)
+# For all the fishes and pseudo absence, we attribute the closest observation (thanks to coordinates) of all of our variables : geomorphology, temperature, salinity, depth
+
+# Importing geomorphic features
+# Ce csv a Ã©tÃ© crÃ©Ã© Ã  partir des donnÃ©es sur https://www.deepreef.org/bathymetry/65-3dgbr-bathy.html 
+# pour chaque fichier shapefile les commandes suivantes ont Ã©tÃ© effectuÃ©es, puis tout a Ã©tÃ© fusionnÃ© : 
+# my_spdf_cay <- readOGR( 
+#   dsn= "3dgbr_geomorph/shape", 
+#   layer = "coralsea_cay",
+#   verbose=FALSE)
+# cc = coordinates(my_spdf_cay)
+# geomorphic_1 = data.frame(cc)
+# colnames(geomorphic_1) = c("lon","lat")
+# geomorphic_1$type = "cay"
+
 
 download.file("http://sdm.dev-lab.net/geomorphic.csv",
               destfile = "geomorphic.csv", mode = "wb")
 geomorphic <- read.table("geomorphic.csv", sep = ',', header = T)
 
+# faire reprÃ©sentation graphique ? 
+# Exemple: on trace les coordonnÃƒÂ©es des points du canyon
+#plot(coordinates(my_spdf_canyon),pch=20)
+# On ajoute le plateau
+#points(coordinates(my_spdf_plateau),pch=17,col="orange")
+# Faire un plot reprÃ©sentant tout le geomorphic ??
+# plot(coordinate(geomorphic))
 
 for (k in 1:nrow(DATA)){
   d = sqrt((DATA$longitude[k]-geomorphic$lon)^2 + (DATA$latitude[k]-geomorphic$lat)^2) 
@@ -396,10 +341,8 @@ for (k in 1:nrow(DATA)){
   DATA$geomorphic[k] = geomorphic$type[i_dmin]
 }
 
-# Importing climatic data for each time period (température et salinité)
-# voir si on n'importe pas le jeu de données directement de NOAH.
-# Ici on a données où les températures sont triées en 3 catégories en fonction de la profondeur dans la mer. Est-ce utile ici ? faire nous même les profondeurs ? voir comment c'est présenté sur NOAH
-# se servir des profondeur d'observation des poissons (qu'on a jarté de notre jeu d'occurance) pour savoir exactement la tempé à ce point d'obs
+# Importing climatic data for each time period (tempÃ©rature et salinitÃ©)
+# Ici on a donnÃ©es oÃ¹ les tempÃ©ratures sont triÃ©es en 3 catÃ©gories en fonction de la profondeur dans la mer
 
 download.file("http://sdm.dev-lab.net/Temperature_1985_1994.RData",
               destfile = "Temperature_1985_1994.RData", mode = "wb")
@@ -410,10 +353,11 @@ Salinity1 <- brick(get(load("Salinity_1985_1994.RData")))
 spplot(Salinity1,names.attr=c("Bottom","0-50m","0-200m"),main="Salinity 1985_1994")
 spplot(Temperature1, names.attr=c("Bottom","0-50m","0-200m"), main="Temperature 1985_1994")
 
-# c'est les large spatialpixel data frame Temperature et Salinity crée en même temps qui nous interessent. 
-# le problème c'est qu'à chaque fois qu'on charge un nouveau raster température (ou salinité) celui là va être changer
-# Ainsi on récupère les données de temperatures et salinité qui nous interessent pour ce decade (pour data frame OCC et ABS), puis on charge un nouveau jeu de données températures (en salinité)
-# les coordonnées des data frame température et salinity sont les mêmes (même pas de données originelle NOAA). Ainsi on peut ne chercher la coord la plus proche qu'une fois
+# c'est les large spatialpixel data frame Temperature et Salinity crÃ©e en mÃªme temps qui nous interessent. 
+# le problÃ¨me c'est qu'Ã  chaque fois qu'on charge un nouveau raster tempÃ©rature (ou salinitÃ©) celui lÃ  va Ãªtre changer
+# Ainsi on rÃ©cupÃ¨re les donnÃ©es de temperatures et salinitÃ© qui nous interessent pour ce decade (pour data frame OCC et ABS), puis on charge un nouveau jeu de donnÃ©es tempÃ©ratures (en salinitÃ©)
+# les coordonnÃ©es des data frame tempÃ©rature et salinity sont les mÃªmes (mÃªme pas de donnÃ©es originelle NOAA). Ainsi on peut ne chercher la coord la plus proche qu'une fois
+# The fish studied lives on the surface of the sea, thus, we will get temperature and salinity of surface water. 
 for (k in 1:nrow(DATA)){
   if (DATA$decade[k] == 1){
     d = sqrt((DATA$longitude[k]-Temperature@coords[,1])^2+ (DATA$latitude[k]-Temperature@coords[,2])^2) 
@@ -466,12 +410,13 @@ for (k in 1:nrow(DATA)){
   }
 }
 
-# On réfléchit à ajouter des données sur les sédiments venant de MARS  http://dbforms.ga.gov.au/pls/www/npm.mars.search
-# mais il faut qu'on étudie plus cet aspect et qu'on décide quelle forme de data on veut (récupérer que des sédiments à la surface)
+# On rÃ©flÃ©chit Ã  ajouter des donnÃ©es sur les sÃ©diments venant de MARS  http://dbforms.ga.gov.au/pls/www/npm.mars.search
+# mais il faut qu'on Ã©tudie plus cet aspect et qu'on dÃ©cide quelle forme de data on veut (rÃ©cupÃ©rer que des sÃ©diments Ã  la surface)
 
 
-# Avant analyse il faudra certainement "neutraliser" les coordonnées pour ne pas qu'elles soient considérées comme une variable explicative
+# Avant analyse il faudra certainement "neutraliser" les coordonnÃ©es pour ne pas qu'elles soient considÃ©rÃ©es comme une variable explicative
 DATA$longitude <- 1:nrow(DATA)
 DATA$latitude <- 1:nrow(DATA) 
 
-# Le tableau est prêt à être analyser ! 
+# Le tableau est prÃªt Ã  Ãªtre analyser ! 
+write.csv(DATA,"DATA.csv", row.names = TRUE)
